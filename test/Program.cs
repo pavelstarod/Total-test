810775db32982536cfde4a5b3375597ed43e376e
Pavel Starodubov <pablostar68@yahoo.com>
2022-12-05 22:55:49 +0300

    add program

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void NewArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"{array[i]}  ");
+        }
+    }
+}
+
+Console.WriteLine("Введите через пробел строки массива: ");
+string[] array = Console.ReadLine().Split();
+Console.WriteLine();
+Console.WriteLine("Вывод строк с 3 и менее символами: ");
+NewArray(array);
