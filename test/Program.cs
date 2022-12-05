be18336816135a66c91bcc467c0f3b897bbed5e2
Pavel Starodubov <pablostar68@yahoo.com>
2022-12-06 01:51:27 +0300

    fix test

--- test/Program.cs
+++ test/Program.cs
@@ -1,16 +1,33 @@
-﻿void NewArray(string[] array)
+﻿void PrintArray(string[] array)
 {
+    for (int i = 0; i < array.Length; i++)
+    {
+        {
+            Console.Write($"{array[i]} ");
+        }        
+    }
+    Console.WriteLine();
+}
+
+void ChangeArray(string[] array, string[] array2)
+{
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            Console.Write($"{array[i]}  ");
+            array2[count] = array[i];
+            count++;
         }
     }
 }
 
+
+
 Console.WriteLine("Введите через пробел строки массива: ");
 string[] array = Console.ReadLine().Split();
+string[] array2 = new string[array.Length];
+ChangeArray(array, array2);
 Console.WriteLine();
-Console.WriteLine("Вывод строк с 3 и менее символами: ");
-NewArray(array);
+Console.WriteLine("Вывод массива с элементами длина которых меньше либо равна 3 символа: ");
+PrintArray(array2);
